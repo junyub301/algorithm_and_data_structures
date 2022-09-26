a793d91763ec3ea52f47c38d42140d7b07d8565e
junyub301 <junyub0301@gmail.com>
2022-09-26 16:27:40 +0900

    Queue

--- src/data_structures/queue.js
+++ src/data_structures/queue.js
@@ -1,55 +1,57 @@
 /* 
-    stack
-    - LIFO (Last In First Out) 
+Queue
+- FIFO(First In First Out)
 
-    stack 작성은 여러가지 방법이 있다.
-    - 배열을 이용 -> push/pop , unshift/shift
+사용처
+- 컴퓨터 백그라운드 작업
+- 인쇄 작업
+ 
+ Queue 작성은 여러가지 방법이 있다.
+    - 배열을 이용 -> push/shift , unshift/pop
     - class를 이용해 직접 구현
     .....
 
-    Big O
-    - insertion -> O(1)
-    - Removal -> O(1)
-    - Searching -> O(N)
-    - Access -> O(N)
+Big O
+- insertion -> O(1)
+- Removal -> O(1)
+- Searching -> O(N)
+- Access -> O(N)
+
 */
 
 class Node {
-    constructor(val) {
-        this.val = val;
+    constructor(value) {
+        this.value = value;
         this.next = null;
     }
 }
 
-class Stack {
+class Queue {
     constructor() {
         this.first = null;
         this.last = null;
         this.size = 0;
     }
 
-    push(val) {
+    enqueue(val) {
         let newNode = new Node(val);
         if (!this.first) {
             this.first = newNode;
             this.last = newNode;
         } else {
-            let temp = this.first;
-            this.first = newNode;
-            this.first.next = temp;
+            this.last.next = newNode;
+            this.last = newNode;
         }
         return ++this.size;
     }
-
-    pop() {
-        if (this.size === 0) return null;
+    dequeue() {
+        if (!this.first) return null;
         if (this.first === this.last) {
             this.last = null;
         }
         let temp = this.first;
         this.first = this.first.next;
         this.size--;
-
-        return temp.val;
+        return temp.value;
     }
 }
